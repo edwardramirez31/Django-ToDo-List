88dd9dc7e8fc54a434fd6b00f65da199f6f19ff4
Edward Ramirez <edal_ramirez@hotmail.com>
2021-01-02 14:08:52 -0500

    Creating a simple url pattern, view and a hello world template

---
+++ todo/tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+app_name = "tasks"
+
+urlpatterns = [
+    path('list', TasksListView.as_view(), name='tasks'),
+]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+
+class TasksListView(View):
+    template_name = 'tasks/list.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
