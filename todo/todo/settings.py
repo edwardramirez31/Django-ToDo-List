bb2b9d4218f1d4235a9971233ee1db38eff287c9
Edward Ramirez <edal_ramirez@hotmail.com>
2021-01-03 08:51:43 -0500

    Creating the logout template and adding more tags to the login template
    In order to display on screen a logout template when the user clicks on the logout anchor tag and it doesn't have
    a next parameter in the url, I had to create a template view that renders the 'registration/logout.html' template.
    By default, Django sends the user to the logout template from the admin site but I don't want the user to
    look at this syte, I want the user to have another template. So, I edited the LOGOUT_REDIRECT_URL in settings.py.
    
    If I want to redirect the user to a wished url (Django sends the user to 'accounts/profile' by default), I also had
    to edit LOGIN_REDIRECT_URL

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# redirigir login cuando no existe el next
+LOGIN_REDIRECT_URL = '/tasks/list'
+# redirigir el logout cuando no hay next
+LOGOUT_REDIRECT_URL = '/logout'
+
+# este se para el login required mixin
+# LOGIN_URL = 'login'
