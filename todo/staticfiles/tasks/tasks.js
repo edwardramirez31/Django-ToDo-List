5d176679b92834e41de0fe1f327bbd83d17dc46c
Edward Ramirez <edal_ramirez@hotmail.com>
2021-03-13 16:00:38 -0500

    Creating the staticfiles folder
    I made this with the 'python manage.py collectstatic' command, so that the web server can serve static files from a single location,
    rather than configure the web server to serve static files from multiple paths.

---
+++ todo/staticfiles/tasks/tasks.js
@@ -0,0 +1,53 @@
+function markTask (url, id) {
+  // MARK AND UNMARK IN THE FRONTEND 
+  var divID = 'Check-Item-' + id;
+  checkItem = document.getElementById(divID);
+  console.log(checkItem);
+  label = checkItem.getElementsByTagName('label')[0];
+  console.log(label.style.textDecoration);
+  if (label.style.textDecoration == false) {
+    label.style.textDecoration = 'line-through';
+  } else {
+    label.style.textDecoration = '';
+  };
+
+  $.post(url);
+}
+
+function deleteTask(url, id) {
+  $('#Check-Item-' + id).parent().remove();
+  $.post(url);
+}
+
+var postURL;
+var labelID;
+
+function updateTask(url, id) {
+  postURL = url;
+  labelID = id;
+}
+
+var modal_btn = document.querySelector('#modal-update');
+modal_btn.addEventListener('click', async (e) => {
+  let modalLabel = document.querySelector("#update-label");
+  var text = modalLabel.value;
+  var label = document.querySelector(`#task-${labelID}`);
+  if (text === '') {
+    $('#taskModal').modal('hide');
+  } else {
+    label.innerHTML = text;
+    modalLabel.value = '';
+    $('#taskModal').modal('hide');
+    var response = await fetch(postURL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        "X-CSRFToken": getCookie('csrftoken')
+      },
+      body: JSON.stringify({"new_title": text})
+    });
+    var data = await response.text();
+    console.log(data);
+  }
+  
+});
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # redirigir login cuando no existe el next
