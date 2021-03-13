452016e279ed93bcc895954ebe24b44c170413e6
Edward Ramirez <edal_ramirez@hotmail.com>
2021-03-13 15:43:09 -0500

    Adding whitenoise and the static root folder
    Heroku doesn't know how to serve static files, so I had to add whitenoise in my settings.py file at the MIDDLEWARE configuration. This
    allows my web app to serve its own static files, making it a self-contained unit that can be deployed anywhere. I also had
    to set in settings where the static files are going to be after Heroku uses the collectstatic command, so I added the STATIC_ROOT
    configuration as 'os.path.join(BASE_DIR, 'staticfiles')'

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +46,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
